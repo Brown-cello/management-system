21b220437f20ffd7ace6c17f1a5f3ddcccc41190
Brown-cello <flexwidflash@gmail.com>
2025-04-01 16:35:53 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [TodoModule, UserModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
