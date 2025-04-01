1834db0a41bdc4f56b0a4b1748e73e5dada2e1b6
Brown-cello <flexwidflash@gmail.com>
2025-04-01 17:21:38 +0100

    saving data in sql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService ,} from '@nestjs/config';
+import { User } from './user/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodoModule, UserModule],
+  imports: [
+     ConfigModule.forRoot({
+      isGlobal:true}), TypeOrmModule.forRootAsync({
+        imports:[ConfigModule],
+        inject:[ConfigService],
+        useFactory:(ConfigService:ConfigService)=>({
+            type:'mysql',
+            host:ConfigService.getOrThrow('DB_HOST'),
+            port:ConfigService.getOrThrow('DB_PORT'),
+            username:ConfigService.getOrThrow('DB_USERNAME'),
+            password:ConfigService.getOrThrow('DB_PASSWORD'),
+            database:ConfigService.getOrThrow('DB_NAME'),
+            entities:[User],
+            synchronize:true
+
+        })
+
+    }),
+    TodoModule, UserModule],
   controllers: [],
   providers: [],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+// import { JwtService } from '@nestjs/jwt';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User )
+    private userRepository:Repository<User >,
+    // private jwtService:JwtService
+  ){}
+  async create(dto:CreateUserDto){
+    // const user = this.findByEmail(dto.email)
+    // if(await user){
+      // throw new BadRequestException('email already exists')
+    // }
+    //  const salt = await bcrypt.genSalt(10);
+    // dto.password = await bcrypt.hash(dto.password, salt);
+
+// const payload = { sub: 'user.id', email: 'user.email' };
+    const newCar = await this.userRepository.create(dto);
+    return{
+     userDetails : await this.userRepository.save(newCar),
+    //  access_token: await this.jwtService.signAsync(payload),
+    }
   }
+  
 
   findAll() {
     return `This action returns all user`;
